d44a63c41c5cd86c66a4b8c9aa5edd971c66fc6b
Paulo <paulo@fazendadosoftware.com>
2021-11-27 05:17:35 -0300

    current state

--- packages/app/src/hooks/useFipe.ts
+++ packages/app/src/hooks/useFipe.ts
@@ -12,14 +12,15 @@ const getFilterQuery = (modelYearFilter: IModelYearFilter, limit?: number) => {
   // 0km
   query.push(zeroKm ? 'year == 32000' : 'year != 32000')
   // vehicleTypeIds
-  if (vehicleTypeIds.size < 3 && vehicleTypeIds.size > 0) query.push([...vehicleTypeIds].map(id => `model.vehicleTypeCode == ${id}`).join(' OR '))
+  if (vehicleTypeIds.size < 3 && vehicleTypeIds.size > 0) query.push(`(${[...vehicleTypeIds].map(id => `model.vehicleTypeCode == ${id}`).join(' OR ')})`)
   // makes
   // if (makeIds.length > 0) query.push(`model.make.id in ${JSON.stringify(makeIds)}`)
-  if (makeIds.length > 0) query.push(makeIds.map(id => `model.make.id == ${id}`).join(' OR '))
+  if (makeIds.length > 0) query.push(`(${makeIds.map(id => `model.make.id == ${id}`).join(' OR ')})`)
   // limit
 
   let _query = query.join(' AND ')
   if (typeof limit === 'number') _query += ` LIMIT(${limit})`
+  console.log('QUERY', _query)
   return _query
 }
 
